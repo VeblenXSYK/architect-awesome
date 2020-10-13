248c24abb868e62b9bb66c9d91cb4aa98c02ae66
VeblenXSYK <1207316368@qq.com>
2020-10-13 22:00:57 +0800

    优化链表反转以及对应的gtest单元测试代码

--- 数据结构+算法/reverselist.cpp
+++ 数据结构+算法/reverselist.cpp
@@ -9,18 +9,41 @@ struct ListNode
 	ListNode(int i) : val(i),next(NULL) {}
 };
 
-void PrintInfo(ListNode *list, int databuf[])
+ListNode *CreateList(int datanum, int databuf[])
+{
+	// 准备链表
+	ListNode *pCur = NULL;
+	ListNode *pHead = NULL;
+	for(int i = 0; i < datanum; i++)
+	{
+		if(i == 0)
+		{
+			pCur = new ListNode(databuf[i]);
+			pHead = pCur;
+		}
+		else
+		{
+			pCur->next = new ListNode(databuf[i]);
+			pCur = pCur->next;
+		}
+	}
+	
+	return pHead;
+}
+
+void GetDataInfo(ListNode *list, int databuf[])
 {
 	int i = 0;
 	while(list != NULL)
 	{
-		printf("%d ", list->val);
 		databuf[i++] = list->val;
 		list = list->next;
 	}
-	printf("\n");
 }
 
+/*
+	基本思想：遍历一遍链表，利用一个辅助指针，存储遍历过程中当前指针指向的下一个元素，然后将当前节点元素的指针反转后，利用已经存储的指针往后面继续遍历。
+*/
 ListNode *ReverseList(ListNode *pNode)
 {
 	ListNode *prev = NULL;
@@ -35,32 +58,18 @@ ListNode *ReverseList(ListNode *pNode)
 	return prev;
 }
 
-TEST(QuickSortTest, ReverseList)
-{
-	int datanum = 10;
-	
+TEST(ReverseListTest, ReverseList)
+{	
 	// 准备链表
-	ListNode *pCur = NULL;
-	ListNode *pHead = NULL;
-	for(int i = 0; i < datanum; i++)
-	{
-		if(i == 0)
-		{
-			pCur = new ListNode(i);
-			pHead = pCur;
-		}
-		else
-		{
-			pCur->next = new ListNode(i);
-			pCur = pCur->next;
-		}
-	}
-
-	int databuf[datanum] = { 0 };
-	int reversedatabuf[datanum] = { 0 };
-	PrintInfo(pHead, databuf);
-	PrintInfo(ReverseList(pHead), reversedatabuf);
+	int databuf[] = { 10, 34, 13, 42, 57 };
+	int datanum = sizeof(databuf);
+	ListNode *pNode = CreateList(datanum, databuf);
 	
+	// 反转链表
+	int reversedatabuf[datanum] = { 0 };
+	GetDataInfo(ReverseList(pNode), reversedatabuf);
+	
+	// 测试
 	for(int i = 0; i < datanum; i++)
 	{
 		EXPECT_EQ(databuf[i], reversedatabuf[datanum - i - 1]);
